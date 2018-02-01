3f98749603649aba719088890928e733360974c6
Emrah Kumru <emrahkumru@live.com>
2018-02-01 14:52:59 +0300

    search function

--- source/scripts/components/cities/cities.js
+++ source/scripts/components/cities/cities.js
@@ -6,19 +6,74 @@
 
 angular.module('cwa.cities', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/cities', {
             templateUrl: 'templates/cities.html',
             controller: 'citiesCtrl'
         });
     }])
 
-    .controller('citiesCtrl', function($scope,Services) {
+    .controller('citiesCtrl', function ($scope, Services) {
 
         $scope.title = 'Welcome to Cities Weather App';
+        $scope.searchStatus = false;
+        $scope.searchResult = [];
+
 
         Services.getJson().then(function (response) {
             $scope.Cities = angular.fromJson(response.data);
         });
 
+
+    })
+    .directive('citySearch', function () {
+
+        return {
+            restrict: 'A',
+            scope: {
+                Cities: '=',
+                searchStatus: '=',
+                searchResult: '='
+            },
+            link: function (scope, element, attrs) {
+
+                scope.$watch(function () {
+
+                    element.bind("keydown keypress", function (event) {
+
+
+                        if (this.value.length <= 3) {
+                            return true;
+                        } else if (scope.$parent.searchStatus) {
+                            scope.$parent.searchStatus = false;
+                            scope.$parent.searchResult = [];
+
+                        }
+                        var _self = this;
+
+                        var result = scope.$parent.Cities.filter(function (city) {
+                            var cityName = city.name.toLowerCase(),
+                                value = _self.value.toLowerCase();
+
+                            return cityName.indexOf(value) > -1
+                        });
+
+                        if (result) {
+                            scope.$parent.searchStatus = true;
+                            scope.$parent.searchResult = result;
+
+                        } else {
+                            scope.$parent.searchStatus = false;
+                            scope.$parent.searchResult = [];
+
+                        }
+                        scope.$apply();
+
+                    });
+
+                });
+
+            }
+
+        }
     });
